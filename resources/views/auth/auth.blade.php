e39f1ea03dda1d5747c08f8495081340965dc1d4
Tu Nombre <derick061020@gmail.com>
2026-06-12 12:13:06 -0500

    iconos

--- resources/views/auth/auth.blade.php
+++ resources/views/auth/auth.blade.php
@@ -8,7 +8,7 @@
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&family=Inter+Tight:wght@500;600;700&display=swap" rel="stylesheet">
-    <link href="https://unpkg.com/primeicons/primeicons.css" rel="stylesheet" />
+    <link href="{{ asset('vendor/primeicons/primeicons.css') }}" rel="stylesheet" />
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/intl-tel-input@29.0.1/dist/css/intlTelInput.css">
     <script src="https://cdn.tailwindcss.com"></script>
     <script>
--- resources/views/checkout.blade.php
+++ resources/views/checkout.blade.php
@@ -8,7 +8,7 @@
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&family=Inter+Tight:wght@500;600;700&display=swap" rel="stylesheet">
-    <link href="https://unpkg.com/primeicons/primeicons.css" rel="stylesheet" />
+    <link href="{{ asset('vendor/primeicons/primeicons.css') }}" rel="stylesheet" />
     <script src="https://cdn.tailwindcss.com"></script>
     <script src="https://js.stripe.com/v3/"></script>
     <script src="https://cdn.jsdelivr.net/npm/canvas-confetti@1.9.3/dist/confetti.browser.min.js"></script>
