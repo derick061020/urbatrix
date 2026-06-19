9650b082a8789fdeafb93706f32497d2e4c23e7c
Tu Nombre <derick061020@gmail.com>
2026-06-19 10:38:37 -0500

    cambios

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -410,7 +410,7 @@
                   <div class="mt-advisor-status">{{ __('Available right now') }}</div>
                 </div>
               </div>
-              <button type="button" class="mt-advisor-chat" onclick="window.location.href='{{ route('dashboard.messages') }}'">
+              <button type="button" class="mt-advisor-chat" onclick="window.open('{{ route('dashboard.messages') }}', '_blank')">
                 {{ __('Chat') }}
                 <svg width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><line x1="5" y1="12" x2="19" y2="12"/><polyline points="12 5 19 12 12 19"/></svg>
               </button>
