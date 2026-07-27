4b14a00638a6ceccd3f1e9e68e6fc189040e86fd
Tu Nombre <derick061020@gmail.com>
2026-07-27 09:04:14 -0500

    TAAMAÑPO DEL TEXTO

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -11,7 +11,7 @@
   <link href="https://fonts.googleapis.com/css2?family=Antonio:wght@400;500;600;700&amp;display=swap" rel="stylesheet">
   <link rel="icon" href="{{ asset('images/favicon-landmass.png') }}" type="image/png">
   <link href="{{ asset('vendor/primeicons/primeicons.css') }}" rel="stylesheet" />
-  <link rel="stylesheet" href="{{ asset('css/style.css') }}?v=25">
+  <link rel="stylesheet" href="{{ asset('css/style.css') }}?v=26">
 </head>
 
 <body data-view="grid">
