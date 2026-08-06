0ba34f2c718273f349cfa3ed90dcea7dea515833
Tu Nombre <derick061020@gmail.com>
2026-08-06 07:35:42 -0500

    cambios con el dropdown , solo para landmass

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1891,7 +1891,8 @@
               <img src="/images/makai-logo.png" alt="{{ __('logo') }}" class="logo-img" style="height:32px;width:auto;max-width:160px;object-fit:contain;">
             </a>
           </div>
-          <button type="button" id="projectsToggle" class="logo-trigger" onclick="toggleProjects()" aria-label="{{ __('Switch project') }}" aria-expanded="false">
+          {{-- Selector de proyectos oculto: por ahora solo existe Bahía Mar --}}
+          <button type="button" id="projectsToggle" class="logo-trigger" onclick="toggleProjects()" aria-label="{{ __('Switch project') }}" aria-expanded="false" style="display:none;" hidden>
             <svg class="chevron-icon" xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="var(--brand)" stroke-width="2.5" stroke-linecap="round" stroke-linejoin="round">
               <polyline points="6 9 12 15 18 9"></polyline>
             </svg>
